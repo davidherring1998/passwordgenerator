b91c44bc034e829d2bbd515c1a57d807c3bd22a0
David H <106282330+davidherring1998@users.noreply.github.com>
2022-08-18 14:22:40 -0500

    intial commit, draft one

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# passwordgenerator
-password generator that generates password by randomly choosing characters based on user input
+# Password Generator Starter Code
+
+# Password Generator Starter Code
+
+This website is designed to generate a password based on user input,
+When you press the generate password button you are prompted with a series of questions including desired length, upper or lower case, numbers or special characters. Using Javascript, a random order of characters based on the user preferences is generated and displayed on the page.
+
+links:
+
+screenshots:
---
+++ script.js
@@ -0,0 +1,99 @@
+//setting shortcut for generate id 
+var generateBtn = document.querySelector("#generate");
+
+//setting event listener for button 
+generateBtn.addEventListener('click', choices);
+
+//making function to be called when button is pushed
+function choices() {
+                //making the inital prompt 
+                let lengthChar = prompt('How many characters would you like?\nChoose between 8-128');
+                let length = lengthChar;
+                if (length != null) {
+
+                //alert if number is not correct
+        if (length < 8 || length > 128)  {
+            alert("Please choose a number between 8 - 128");
+            return;
+        }
+                //making comfim prompts 
+                    const upperCaseChar = confirm("Do you want uppercase?\n Default: lowercase")
+                    const number = confirm("Do you want numbers?")
+                    const symbols = confirm("Do you want special characters?")
+                // making the diffrent variations
+                    if (upperCaseChar === true && number === true && symbols === true) {
+                    const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+                    let password = ""
+                // creating random number to choose random characters
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+}
+                  } else if (upperCaseChar === true && number === true && symbols != true) {
+                    const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+}       
+                 }  else if (upperCaseChar === true && number != true && symbols != true) {
+                    const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+
+                }   }   else if (upperCaseChar === true && number != true && symbols === true){
+                    const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+
+                }   }   else if (upperCaseChar === true && number === true && symbols != true){
+                    const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password; 
+
+           }   }    else if (upperCaseChar != true && number != true && symbols != true){
+                    const upperCase = 'abcdefghijklmnopqrstuvwxyz';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+
+            }   }   else if (upperCaseChar != true && number === true && symbols === true){
+                    const upperCase = '0123456789!@#$%^&*()_+';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+
+            }   }   else if (upperCaseChar != true && number != true && symbols === true){
+                    const upperCase = '!@#$%^&*()_+';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+
+                }   }   else if (upperCaseChar != true && number === true && symbols != true){
+                    const upperCase = '0123456789';
+                    let password = ""
+                    for (let i = 0; i < length; i++) {
+                    let randomNum = Math.floor(Math.random() * upperCase.length);
+                    password += upperCase.substring(randomNum, randomNum + 1);
+                    document.getElementById('password').innerHTML = password;
+
+ }
+ }
+}}
